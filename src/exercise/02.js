05bfd504a099d030c3510803929307f5f024199e
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-02-15 12:55:35 -0300

    refactor: replace useMemo with useAsync for improved item retrieval in App component

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,10 +1,10 @@
 // useMemo for expensive calculations
-// http://localhost:3000/isolated/exercise/02.js
+// http://localhost:3000/isolated/final/02.extra-2.js
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import {getItems} from '../workerized-filter-cities'
+import {useAsync, useForceRerender} from '../utils'
 
 function Menu({
   items,
@@ -59,7 +59,11 @@ function ListItem({
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
-  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
+
+  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
   const items = allItems.slice(0, 100)
 
   const {
